96d3a3d80a9c535ade0e7320bd353de0b9975e65
Jacob McSwain <jacob.a.mcswain@gmail.com>
2017-08-17 11:55:14 -0500

    Revert "Add ZZMoove Governor"
    This reverts commit 4449335030ff3bc170fae1580f69640fd5c2d65e.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -540,9 +540,6 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
-extern struct cpufreq_governor cpufreq_gov_zzmoove;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
